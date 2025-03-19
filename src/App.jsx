f1399bb24d2f2c7c12b1e0cba2e152ca5a3af8cb
Abel1002 <abelst1002@gmail.com>
2025-03-19 18:10:35 +0100

    incial

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1>Hello, World!</h1>
+      <p>Bienvenido a mi web para la ponencia sobre Inteligencia Artificial.</p>
+    </div>
+  );
+}
+
+export default App;
